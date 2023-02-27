5e2dd87e35eadae600574c827961b83df2602658
BuzzKill Master <BuzzKillMaster@proton.me>
2023-02-27 08:33:08 +0100

    rename dropdown menu option and related variables

--- src/components/FilterDropdown.tsx
+++ src/components/FilterDropdown.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import {IoMdArrowDropdown} from "react-icons/io";
 import {AnimatePresence, motion} from "framer-motion";
-import DropdownMenuItem from "@/components/DropdownMenuOption";
+import DropdownMenuItem from "@/components/DropdownMenuItem";
 
 const continents = [
     "Any",
@@ -18,18 +18,18 @@ export default function FilterDropdown(props: {
     value: string
     handleChange: (value: string) => void
 }) {
-    const [showOptions, setShowOptions] = useState(false)
+    const [showDropdown, setShowDropdown] = useState(false)
 
     return (
-        <div onClick={() => setShowOptions(!showOptions)} className={"p-4 bg-gray-50 shadow rounded cursor-pointer relative"}>
+        <div onClick={() => setShowDropdown(!showDropdown)} className={"p-4 bg-gray-50 shadow rounded cursor-pointer relative"}>
             <div className={"h-full w-full absolute inset-0"}></div>
             <div className={"flex items-center gap-4"}>
                 <p className={"flex-grow"}>{props.value === "" ? "Filter by continent" : props.value}</p>
-                <IoMdArrowDropdown className={showOptions ? "rotate-180" : ""}/>
+                <IoMdArrowDropdown className={showDropdown ? "rotate-180" : ""}/>
             </div>
 
             <AnimatePresence>
-                {showOptions &&
+                {showDropdown &&
                     <motion.ul initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className={"bg-gray-50 rounded shadow min-w-full w-max top-full mt-2 p-4 right-0 absolute"}>
                         {continents.map(value => (
                             <DropdownMenuItem key={value} onClick={props.handleChange} text={value}/>
